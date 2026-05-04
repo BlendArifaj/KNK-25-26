92313707b241cb4f92b688f2c16d95583a13ff1c
Blend Arifaj <blendarifaj@hotmail.com>
2026-05-04 13:49:09 +0200

    Ushtrimet ne KNK

---
+++ main/java/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private void handleLogin(){
+
+    }
+
+    @FXML
+    private void handleForgotPassword(){
+
+    }
+}
